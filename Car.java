fe9d0e7c5fd26785ffafc21098d15572fd8f8be2
Navineeth <108993563+Navineeth@users.noreply.github.com>
2022-07-09 22:26:14 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,9 @@
+package week1.day1;
+
+public class Car {
+	byte wheel = 4;
+	short speed = 12399;
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Mobile.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Mobile {
+public void makeCall() {
+	System.out.println("I am going to call my friend");
+}
+public void sendMsg() {
+	System.out.println("Hello");
+}
+public static void main(String[] args) {
+	System.out.println("This is my mobile");
+	
+}
+}
